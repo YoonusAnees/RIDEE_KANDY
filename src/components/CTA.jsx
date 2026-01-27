8f76544336dc072ccf5ddc0ef91b8212fe61c009
Yoonus Anees <140184122+YoonusAnees@users.noreply.github.com>
2026-01-27 14:31:28 +0530

    Feat Added lines to CTA

--- src/components/CTA.jsx
+++ src/components/CTA.jsx
@@ -7,23 +7,36 @@ export default function CTA() {
         <div className="font-source">
           <div
             className="absolute
-      w-[36vw] h-[99vh]
-      bg-[url('/src/assets/Rectangle-4.png')]
-      bg-no-repeat bg-center bg-contain
-      md:rotate-315 md:-top-1/3 md:-right-1/28
-      rotate-300
-      -top-80 -right-10
-       "
+            w-[36vw] h-[99vh]
+            bg-[url('/src/assets/Rectangle-4.png')]
+            bg-no-repeat bg-center bg-contain
+            md:rotate-315 md:-top-1/3 md:-right-1/28
+            rotate-300
+            -top-80 -right-10"
           ></div>
+             {/* Horizontel */}
+          <div className="absolute flex p-5 w-full mt-20 md:ml-19">
+            <div
+              className="absolute
+              border-t-3 border-gray-200 p-5 w-3/6 h-screen right-1 mr-3 mt-1 md:mt-0  md:mr-35
+    "
+            ></div>
+          </div>
+            {/* Vertical */}
+          <div className="absolute flex p-5 mt-20 md:mr-21 right-1">
+            <div
+              className="absolute
+      border-r-3 border-gray-200 p-5 w-3/19  md:h-76  h-1/6
+    "
+            ></div>
+          </div>
 
           <div
-            className="
-    absolute
-    top-0
-    w-[45vw] h-[100vh]
-    bg-gray-500/10
-    z-10
-  "
+            className="absolute
+            top-0
+            w-[45vw] h-[100vh]
+          bg-gray-500/10
+            z-10 "
           ></div>
 
           <h1 className="text-center text-3xl p-6 md:px-12 md:mt-20">
@@ -37,19 +50,6 @@ export default function CTA() {
           traditions, and the gentle beauty of a place that feels like home, yet
           unlike anywhere else.
         </div>
-
-        <div
-          className="
-    absolute
-    top-[5%]
-    left-1/2
-    -translate-x-1/2
-    w-[80%]
-    h-[45%]
-    border-t-2 border-r-2 border-black/20
-    z-20
-  "
-        ></div>
 
         <div className="flex justify-center items-center  p-5 md:px-2 md:py-4  ">
           <div className="bg-black flex items-center justify-center border-2 md:w-[15vw] w-[30vw] h-[8vh] font-source text-xl text-white text-center ">
